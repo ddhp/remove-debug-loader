a436e5cd3fd2dd129832b93a1ec2e7e34d933920
JesseChen <jessechen@KKBOXdeMacBook-Pro-199.local>
2017-10-19 11:40:11 +0800

    remove `const methodName = ANYWORDS` pattern

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const loaderUtils = require('loader-utils')
 const requireRegexp = new RegExp(/.*?require\(\s*[\'"]debug[\'"]\s*\).*/, 'g')
 const importRegexp = new RegExp(/^import\s{\s*.+\s*}\sfrom\s[\'"]debug[\'"].*/, 'g')
 
-function genDebugRegexpWithMethodname(methodname) {
-  return new RegExp('.*' + methodname + '\\(\.*?\\)\.*', 'g')
+function genDebugRegexpWithMethodname(methodName) {
+  return new RegExp('.*' + methodName + '(\\(\.*?\\)|\\s?=\\s?)\.*', 'g')
 }
 
 function replaceWithRegexp(source, regexp) {
